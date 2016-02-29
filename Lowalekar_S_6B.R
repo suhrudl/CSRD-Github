01f157e53b04c04a440775ec1cc610b981afd255
suhrudl <suhrudlowalekar@yahoo.in>
2016-02-29 09:35:05 -0500

    update to web scraper
    added function, made scraper generic for setlist.fm

--- Lowalekar_S_6B.R
+++ Lowalekar_S_6B.R
@@ -1,26 +1,58 @@
+rm(list = ls())
 setwd("~/Desktop/Coursework/Spring 16/Collect Store and Ret Data/Week 5:6")
 library(RCurl)
 library(XML)
 library(scrapeR)
 
-#setlist.fm is a website that has statistics about bands and their tours
-#i am collecting data about a band called Tool
-#the data frame has to display the song title and the number of times it has been played live
-#this can be seen on the URL http://www.setlist.fm/stats/tool-2bd6d836.html
+#setlist.fm is a website that has statistics about bands and their tour setlists
+#my function scrapes stats about any band profile on the setlist.fm artist 'tour statistics' tab
+#the data frame displays the song title and the number of times it has been played live in their concerts
+#an example URL: "http://www.setlist.fm/stats/tool-2bd6d836.html" 
+#or "http://www.setlist.fm/stats/metallica-3bd680c8.html"
 
-webpage <- getURL(url = "http://www.setlist.fm/stats/tool-2bd6d836.html")
-webpage <- readLines(tc <- textConnection(webpage))
 
-pagetree <- htmlTreeParse(webpage,useInternalNodes = T)
 
-pagetree
+dataScraper <- function(URL){
+  #get the webpage from URL using a GET request
+  webpage <- getURL(url = URL)
+  #read webpage line by line 
+  webpage <- readLines(tc <- textConnection(webpage))
+  #parse the html structure
+  pagetree <- htmlTreeParse(webpage,useInternalNodes = T)
+  
+  #look for songnames, store them in vector x
+  x <- unlist(xpathApply(pagetree,"//*/td[@class = 'songName']", xmlValue))
+  #look for songcount, store in vector y
+  y <- unlist(xpathApply(pagetree,"//*/td[@class = 'songCount']", xmlValue))
+  #clean up data in x
+  x <- gsub("\n|Play Video|stats","", x)
+  #create dataframe
+  artistStats <<- data.frame(x, y)
+  #look for the band name
+  bandName <- unlist(xpathApply(pagetree,"//*/title", xmlValue))
+  bandName <- gsub("\\Tour Statistics | setlist.fm","", bandName)
+  #add column names
+  colnames(artistStats) <<- c(paste(bandName,"Song Title"), "Live Play Count")
+  return(artistStats)
+  
+}
 
-x <- unlist(xpathApply(pagetree,"//*/td[@class = 'songName']", xmlValue))
-y <- unlist(xpathApply(pagetree,"//*/td[@class = 'songCount']", xmlValue))
+#pass the URL, test band Tool
+URL <- "http://www.setlist.fm/stats/tool-2bd6d836.html"
+dataScraper(URL)
 
-x <- gsub("\n|Play Video|stats","", x)
+#pass different URL, test band Metallica
+URL <- "http://www.setlist.fm/stats/metallica-3bd680c8.html"
+dataScraper(URL)
 
-toolSongs <- data.frame(x, y)
-colnames(toolSongs) <- c("Song Title", "Live Play Count")
+#pass last test URL, test band Pink Floyd. This has a space in band name, checking if code breaks
+URL <- "http://www.setlist.fm/stats/pink-floyd-13d6adc5.html"
+dataScraper(URL)
 
-toolSongs
+#as can be seen on the website, there pink floyd have played 139 distinct songs live.
+#testing for consistency
+
+length(artistStats$`Live Play Count`)
+#[1] 139
+#we can manually compare the values for consistency as well
+
